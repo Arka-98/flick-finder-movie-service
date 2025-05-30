67a5bd8b7a62c2a67205fe56a6eb1d9008ca0b76
Arkadipta <dasarkadipta@gmail.com>
2025-05-31 00:44:50 +0530

    fix: use event pattern instead of message pattern

--- src/consumers/user/user.consumer.ts
+++ src/consumers/user/user.consumer.ts
@@ -1,6 +1,6 @@
-import { IUserEvent, Public, TOPICS, UserEventDto } from '@flick-finder/common';
+import { Public, TOPICS, UserEventDto } from '@flick-finder/common';
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { EventPattern, Payload } from '@nestjs/microservices';
 import { UserConsumerService } from './user.service';
 
 @Public()
@@ -8,19 +8,19 @@ import { UserConsumerService } from './user.service';
 export class UserConsumerController {
   constructor(private readonly consumerService: UserConsumerService) {}
 
-  @MessagePattern(TOPICS.USER.CREATED)
+  @EventPattern(TOPICS.USER.CREATED)
   async handleUserCreated(@Payload() user: UserEventDto) {
     return this.consumerService.createUser(user);
   }
 
-  @MessagePattern(TOPICS.USER.UPDATED)
+  @EventPattern(TOPICS.USER.UPDATED)
   async handleUserUpdated(@Payload() updatedUser: UserEventDto) {
     const { _id: userId, ...userUpdateBody } = updatedUser;
 
     return this.consumerService.updateUser(userId, userUpdateBody);
   }
 
-  @MessagePattern(TOPICS.USER.DELETED)
+  @EventPattern(TOPICS.USER.DELETED)
   async handleUserDeleted(@Payload() userId: string) {
     return this.consumerService.deleteUser(userId);
   }
